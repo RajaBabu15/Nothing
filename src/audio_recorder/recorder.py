4281b6f4992dbf9d6f04473952bcd78a42d26817
Raja Babu <100289530+RajaBabu15@users.noreply.github.com>
2024-08-01 00:09:42 +0530

    #Video Recorder Callback function Added with default duration

--- src/audio_recorder/recorder.py
+++ src/audio_recorder/recorder.py
@@ -3,24 +3,32 @@ import wave
 import threading
 import time
 import os
-from .utils import get_unique_filename
+from .utils import get_unique_filename, create_output_directory
 
 class AudioRecorder:
-    def __init__(self, rate=44100, frames_per_buffer=1024, channels=2, format=pyaudio.paInt16, 
-                 filename="temp_audio.wav", directory="recordings", frame_callback=None, duration=10, stop_callback=None):
+    def __init__(self, rate=44100, 
+                    frames_per_buffer=1024, 
+                    channels=2, 
+                    format=pyaudio.paInt16, 
+                    filename="temp_audio.wav", 
+                    directory="recordings", 
+                    frame_callback=None, 
+                    duration=10, 
+                    stop_callback=None):
         self.open = False
         self.rate = rate
         self.frames_per_buffer = frames_per_buffer
         self.channels = channels
         self.format = format
         self.directory = directory
+        create_output_directory(self.directory)
         self.filename = get_unique_filename(filename)
         self.full_path = os.path.join(self.directory, self.filename)
         self.frame_callback = frame_callback
         self.duration = duration
         self.stop_callback = stop_callback
         self.audio = pyaudio.PyAudio()
-        self.stream = None
+        self.audio_stream = None
         self.audio_frames = []
         self.start_time = time.time()
         self.thread = None
@@ -30,60 +38,57 @@ class AudioRecorder:
 
     def record(self):
         try:
-            self.stream = self.audio.open(format=self.format,
-                                          channels=self.channels,
-                                          rate=self.rate,
-                                          input=True,
-                                          frames_per_buffer=self.frames_per_buffer)
-            self.stream.start_stream()
+            self.audio_stream = self.audio.open(format=self.format,
+                                            channels=self.channels,
+                                            rate=self.rate,
+                                            input=True,
+                                            frames_per_buffer=self.frames_per_buffer)
+            self.audio_stream.start_stream()
             self.open = True
             start_time = time.time()
-            
+
             if self.open:
-                print("Recording...")
+                print("Audio Recording Started")
                 self.audio_frames.clear()
 
             while self.open:
-                data = self.stream.read(self.frames_per_buffer)
+                data = self.audio_stream.read(self.frames_per_buffer)
                 self.audio_frames.append(data)
-
                 elapsed_time = time.time() - start_time
-
                 if self.frame_callback:
                     self.frame_callback(data, self.frames_per_buffer, elapsed_time, self.audio_frames)
-
                 if self.stop_callback and self.stop_callback(data, self.frames_per_buffer, elapsed_time, self.audio_frames):
                     break
-
                 if not self.stop_callback and elapsed_time >= self.duration:
                     break
+
         except Exception as e:
-            print(f"An error occurred during recording: {e}")
+            print(f"An error occurred during audio recording: {e}")
         finally:
             self.stop()
 
     def stop(self):
         if self.open:
             self.open = False
-            if self.stream:
-                self.stream.stop_stream()
-                self.stream.close()
+            if self.audio_stream:
+                self.audio_stream.stop_stream()
+                self.audio_stream.close()
             self.audio.terminate()
-            print("Recording complete!")
+            print("Audio Recording complete!")
 
-            print(f"Saving recording to: {self.full_path}")
+            print(f"Audio Saving recording to: {self.full_path}")
 
             with wave.open(self.full_path, 'wb') as waveFile:
                 waveFile.setnchannels(self.channels)
                 waveFile.setsampwidth(self.audio.get_sample_size(self.format))
                 waveFile.setframerate(self.rate)
                 waveFile.writeframes(b''.join(self.audio_frames))
-            print("Recording saved successfully!")
+            print("Audio Recording saved successfully!")
 
     def start(self):
         self.thread = threading.Thread(target=self.record)
         self.thread.start()
-        self.thread.join()  # This will make the start method block until recording is complete
+        self.thread.join()  
 
     def __enter__(self):
         self.start()
--- src/audio_recorder/utils.py
+++ src/audio_recorder/utils.py
@@ -1,4 +1,5 @@
 import os
+import logging
 
 def get_unique_filename(filename):
     base, extension = os.path.splitext(filename)
@@ -8,3 +9,8 @@ def get_unique_filename(filename):
         new_filename = f"{base}_{counter}{extension}"
         counter += 1
     return new_filename
+
+def create_output_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    logging.info(f"Created directory: {directory}")
